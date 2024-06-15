8279693b4be6d426261f21851bac219d5193cdf5
Chan <qufstar46@gmail.com>
2024-06-15 23:33:13 +0900

    feat: remove slug pages for server rendering

--- app/[[...slug]]/client.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-import React from "react";
-
-const App = dynamic(() => import("../../src/App"), { ssr: false });
-
-export default function ClientOnly() {
-  return <App />;
-}
