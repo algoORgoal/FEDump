bd5dfbfa2ddd0c13ccec28ddc9029e7e9bebdbdf
Chan <qufstar46@gmail.com>
2024-06-18 02:20:21 +0900

    feat: modify title max length for a full view

--- src/entities/post/lib/trimTitle.ts
+++ src/entities/post/lib/trimTitle.ts
@@ -1,5 +1,5 @@
 export const trimTitle = (title: string) => {
-  const MAX_LENGTH = 50;
+  const MAX_LENGTH = 80;
   const trimmedTitle =
     title.length > MAX_LENGTH ? title.substring(0, MAX_LENGTH) + "..." : title;
 
