9eeaf7165f3faf9324ae7761e4dfe025bbf0c80e
Chan <qufstar46@gmail.com>
2024-06-18 17:44:01 +0900

    refactor: use ol element semantic markup

--- app/posts/review-no-silver-bullet-software-engineering-reloaded/layout.tsx
+++ app/posts/review-no-silver-bullet-software-engineering-reloaded/layout.tsx
@@ -9,5 +9,5 @@ export const metadata: Metadata & {
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return <article>{children}</article>;
 }
