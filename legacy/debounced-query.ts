688f6f8ca2a2a17582269c3921480ec045bf0cd0
Chan <qufstar46@gmail.com>
2024-06-19 12:29:11 +0900

    chore: remove legacy files

--- legacy/debounced-query.ts
+++
@@ -1,9 +0,0 @@
-import { useSearchStore } from "../src/features/search";
-import { useDebounce } from "../src/shared/model/useDebounce";
-
-export const useDebouncedQuery = () => {
-  const query = useSearchStore((state) => state.query);
-  const debouncedQuery = useDebounce(query, 500);
-
-  return debouncedQuery;
-};
