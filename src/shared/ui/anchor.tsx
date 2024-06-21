b4f4a644e982d40554d082f8fb7e26de90504f6d
Chan <qufstar46@gmail.com>
2024-06-21 17:22:51 +0900

    chore: remove console statement

--- src/shared/ui/anchor.tsx
+++ src/shared/ui/anchor.tsx
@@ -1,6 +1,5 @@
 import React, { ComponentPropsWithoutRef } from "react";
 
 export default function Anchor(props: ComponentPropsWithoutRef<"a">) {
-  console.dir(props);
   return <a {...props} className="hover:underline" />;
 }
