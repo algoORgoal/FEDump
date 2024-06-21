b203686bd5292337b12d32d975b8ffbecfe8b9ab
Chan <qufstar46@gmail.com>
2024-06-21 15:03:08 +0900

    style: place horizontal margin on ever page'

--- app/(posts)/layout.tsx
+++ app/(posts)/layout.tsx
@@ -4,7 +4,7 @@ import { Header } from "@/src/widget/post/ui/Header";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto w-full md:w-[768px]">
+    <div className="box-border w-full px-4 md:mx-auto md:w-[768px]">
       <Header />
       {children}
     </div>
