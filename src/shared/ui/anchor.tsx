391f109244868afee8bbcf64d4f3d83154fa91fd
Chan <qufstar46@gmail.com>
2024-06-21 17:18:26 +0900

    style: appear underlines only on hover

--- src/shared/ui/anchor.tsx
+++ src/shared/ui/anchor.tsx
@@ -1,5 +1,6 @@
 import React, { ComponentPropsWithoutRef } from "react";
 
 export default function Anchor(props: ComponentPropsWithoutRef<"a">) {
-  return <a {...props} className="cursor-pointer text-slate-500 underline" />;
+  console.dir(props);
+  return <a {...props} className="hover:underline" />;
 }
