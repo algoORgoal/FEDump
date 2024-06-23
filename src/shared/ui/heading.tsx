d656bc16116da710cf9880223ed315337e45273b
Chan <qufstar46@gmail.com>
2024-06-24 00:50:16 +0900

    style: remove seperator and add margin

--- src/shared/ui/heading.tsx
+++ src/shared/ui/heading.tsx
@@ -1,51 +1,34 @@
 import { ComponentPropsWithoutRef, ReactNode } from "react";
 import { Separator } from "./separator";
 
-function BaseHeading({ children }: { children: ReactNode }) {
-  return (
-    <>
-      {children}
-      <Separator />
-    </>
-  );
-}
-
 export function Heading1(props: ComponentPropsWithoutRef<"h1">) {
   return (
-    <div className="my-10 space-y-7">
-      <BaseHeading>
-        <h1 className="text-4xl font-extrabold text-black" {...props} />
-      </BaseHeading>
+    <div className="my-10">
+      <h1 className="text-7xl font-extrabold text-black" {...props} />
     </div>
   );
 }
 
 export function Heading2(props: ComponentPropsWithoutRef<"h2">) {
   return (
-    <div className="my-7 space-y-5">
-      <BaseHeading>
-        <h2 className="text-3xl font-bold text-black" {...props} />
-      </BaseHeading>
+    <div className="mb-12 mt-24">
+      <h2 className="text-5xl font-bold text-black" {...props} />
     </div>
   );
 }
 
 export function Heading3(props: ComponentPropsWithoutRef<"h3">) {
   return (
-    <div className="my-5 space-y-3">
-      <BaseHeading>
-        <h3 className="text-2xl font-bold text-black" {...props} />
-      </BaseHeading>
+    <div className="mb-8 mt-16">
+      <h3 className="text-3xl font-bold text-black" {...props} />
     </div>
   );
 }
 
 export function Heading4(props: ComponentPropsWithoutRef<"h4">) {
   return (
-    <div className="my-4 space-y-2">
-      <BaseHeading>
-        <h4 className="text-xl font-bold text-black" {...props} />
-      </BaseHeading>
+    <div className="mb-5 mt-11">
+      <h4 className="text-xl font-bold text-black" {...props} />
     </div>
   );
 }
